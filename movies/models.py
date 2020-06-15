704c580410cf8afd5ba874c9f8185dbe3a5230a8
username <email@email.com>
2020-06-15 12:01:07 +0900

    Create | Review URL

--- movies/models.py
+++ movies/models.py
@@ -29,3 +29,11 @@ class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews', blank=True)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
