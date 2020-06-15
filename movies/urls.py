eaec350e7a68a3ee0c974e8a295467dd504af3c3
username <email@email.com>
2020-06-15 15:14:43 +0900

    Create, Update, Delete | Review

--- movies/urls.py
+++ movies/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('recommend/', views.recommend, name='recommend'),
     path('<int:movie_pk>/', views.movie_detail, name='movie_detail'),
-    path('<int:movie_pk/create_review/', views.create_review, name='create_review')
-    path('<int:movie_pk/<int:review_pk>/update/', views.update_review, name='update_review'),
-    path('<int:movie_pk/<int:review_pk>/delete/', views.delete_review, name='delete_review'),
+    # path('<int:movie_pk>/create_review/', views.create_review, name='create_review'),
+    path('<int:movie_pk>/<int:review_pk>/update/', views.update_review, name='update_review'),
+    path('<int:movie_pk>/<int:review_pk>/delete/', views.delete_review, name='delete_review'),
 ]
 
--- movies/views.py
+++ movies/views.py
@@ -21,37 +21,88 @@ def index(request):
     }
     return render(request, 'movies/index.html', context)
 
-def movielist(request, movielist_pk):
-    recommend = Recommend.objects.get(pk=recommend_pk)
-    movies = Movie.objects.filter(genres=recommend.genre, vote_average__gte=recommend.vote_average)
 
-    paginator = Paginator(movies, 6)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        'movies': movies,
-        'page_number': page_number,
-        'page_obj' : page_obj,
-    }
-    return render(request, 'movies/recommend_list.html', context)
+# def movielist(request, movielist_pk):
+#     recommend = Recommend.objects.get(pk=recommend_pk)
+#     movies = Movie.objects.filter(genres=recommend.genre, vote_average__gte=recommend.vote_average)
 
-    movies = Movie.objects.order_by('pk')[:6]
-    context = {
-        'movies': movies,
-    }
-    return render(request, 'movies/index.html', context)
+#     paginator = Paginator(movies, 6)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     context = {
+#         'movies': movies,
+#         'page_number': page_number,
+#         'page_obj' : page_obj,
+#     }
+#     return render(request, 'movies/recommend_list.html', context)
 
+#     movies = Movie.objects.order_by('pk')[:6]
+#     context = {
+#         'movies': movies,
+#     }
+#     return render(request, 'movies/index.html', context)
 
+@login_required
 def movie_detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
-    form = ReviewForm()
     reviews = Review.objects.order_by('-pk')
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.movie = movie
+            review.save()
+            return redirect('movies:movie_detail', movie_pk)
+    else:
+        form = ReviewForm()
     context = {
         'movie': movie,
         'form': form,
         'reviews': reviews,
     }
-    return render(requestm, 'movies/movie_detail.html', context)
+    return render(request, 'movies/movie_detail.html', context)
+
+
+# @login_required
+# def create_review(request, movie_pk):
+#     movie = get_object_or_404(Movie, pk=movie_pk)
+#     if request.method == 'POST':
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             review = form.save(commit=False)
+#             review.user = request.user
+#             review.movie = movie
+#             review.save()
+#             return redirect('movies:movie_detail', movie_pk)
+
+
+@login_required
+def update_review(request, movie_pk, review_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    review = get_object_or_404(Review, pk=review_pk)
+    if request.user == review.user:
+        if request.method == 'POST':
+            form = ReviewForm(request.POST, instance=review)
+            if form.is_valid():
+                form.save()
+                return redirect('movies:movie_detail', movie_pk)
+        else:
+            form = ReviewForm(instance=review)
+        context = {
+            'form': form,
+        }
+        return render(request, 'movies/update_review.html', context)
+    else:
+        return redirect('movies:movie_detail', movie_pk)
+
+@require_POST
+@login_required
+def delete_review(request, movie_pk, review_pk):
+    review = get_object_or_404(Review, pk=review_pk)
+    if request.user == review.user:
+        review.delete()
+    return redirect('movies:movie_detail', movie_pk)
 
 
 def recommend(request):
